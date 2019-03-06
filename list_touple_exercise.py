930c0b71b60de29ebcc0d85d8a4ce266ae8c4a0a
bvash <bibhashpal342@gmail.com>
2019-03-06 10:29:50 +0545

    initial

---
+++ list_touple_exercise.py
@@ -0,0 +1,9 @@
+#Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.
+#a=1,2,3,4,5,6,7,8,9
+#print list(a)
+#print tuple(a)
+
+#Write a Python program to display the first and last colors from the following list. 
+#colcor_list=["red","green","white","black"]
+#print colcor_list[0],colcor_list[-1]
+
